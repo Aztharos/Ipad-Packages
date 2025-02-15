5ec908c00b63ed6e4b197f9d39e4884cb3bb64bb
Aztharos <166838533+Aztharos@users.noreply.github.com>
2025-02-15 14:57:11 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,17 +7,17 @@ let package = Package(
         .iOS(.v13),
         .macOS(.v10_15)
     ],
+        products: [
+        .library(
+            name: "MonPackageSwift", 
+            targets: ["MonPackageSwift"]
+        )
+    ],
     dependencies: [
         .package(url: "https://github.com/Aztharos/swifter.git", .exact("1.5.0")),
         .package(url: "https://github.com/Aztharos/Ink.git", .exact("0.6.0")),
         .package(url: "https://github.com/Aztharos/Highlightr.git", .exact("2.2.1")),
         .package(url: "https://github.com/Aztharos/SwiftSoup.git", .exact("2.7.7"))
-    ],
-    products: [
-        .library(
-            name: "MonPackageSwift", 
-            targets: ["MonPackageSwift"]
-        )
     ],
     targets: [
         .target(
