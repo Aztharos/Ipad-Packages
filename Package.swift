610c3d2fe1cf0e9f2c3ca738fa40577a0c08f696
Aztharos <166838533+Aztharos@users.noreply.github.com>
2025-02-15 12:12:22 +0100

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "MonPackageSwift",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15)
+    ],
+    dependencies: [
+        .package(url: "https://github.com/httpswift/swifter.git", from: "1.0.0"),
+        .package(url: "https://github.com/JohnSundell/Ink.git", from: "2.0.0"),
+        .package(url: "https://github.com/raspu/Highlightr.git", from: "2.0.0"),
+        .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.0.0")
+    ],
+    targets: [
+        .target(
+            name: "MonPackageSwift",
+            dependencies: [
+                "Swifter",
+                "Ink",
+                "Highlightr",
+                "SwiftSoup"
+            ]
+        ),
+        .testTarget(
+            name: "MonPackageSwiftTests",
+            dependencies: ["MonPackageSwift"]
+        )
+    ]
+)
