0d983c7c937439c700c6bf20fc84c0394d5c7fd9
Aztharos <166838533+Aztharos@users.noreply.github.com>
2025-02-15 13:25:11 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Aztharos/swifter.git", .exact("V1.5.0")),
         .package(url: "https://github.com/Aztharos/Ink.git", .exact("0.6.0")),
-        .package(url: "https://github.com/raspu/Highlightr.git", from: "2.0.0"),
+        .package(url: "https://github.com/Aztharos/Highlightr.git", .exact("2.2.1")),
         .package(url: "https://github.com/Aztharos/SwiftSoup.git", .exact("2.7.7"))
     ],
     targets: [
